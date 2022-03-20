e3f408d62c9fd9f24d277e958334b195dfbe544b
Konstantin <slider78@rambler.ru>
2022-03-21 00:32:54 +0500

    tests

--- 16-mvc/src/test/resources/data.sql
+++
@@ -1,4 +0,0 @@
-
-insert into comments (id, text, book, datetime) values (4, 'not bad', 2, CAST('2022-02-09 12:35:29' AS datetime));
-insert into comments (id, text, book, datetime) values (5, 'so so', 2, CAST('2022-02-10 12:36:29' AS datetime));
-insert into comments (id, text, book, datetime) values (6, 'thumbs up', 1, CAST('2022-02-11 12:37:29' AS datetime));
