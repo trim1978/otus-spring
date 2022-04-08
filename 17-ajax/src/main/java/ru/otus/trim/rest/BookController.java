f6e6aacc6c7e31c84b608b4c7753854ec0744bf1
Konstantin <slider78@rambler.ru>
2022-04-08 21:26:50 +0500

    17-ajax

--- 17-ajax/src/main/java/ru/otus/trim/rest/BookController.java
+++ 17-ajax/src/main/java/ru/otus/trim/rest/BookController.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.otus.trim.model.Author;
 import ru.otus.trim.model.Book;
 import ru.otus.trim.model.Genre;
@@ -29,11 +26,12 @@ public class BookController {
     private final LibraryService library;
 
     @GetMapping("/api/books")
-    public List<BookDto> getBooksByPage(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {{
-        Pageable pageable = PageRequest.of(page, 10);
+    public List<BookDto> getBooksByPage(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "step", defaultValue = "10") int pageCapacity, Model model) {{
+        Pageable pageable = PageRequest.of(page, pageCapacity);
         Page<Book> books = library.getBooks(pageable);
+        model.addAttribute("page_capacity", pageCapacity);
         model.addAttribute("page", page);
-        model.addAttribute("total", books.getTotalPages());
+        model.addAttribute("total", books.getTotalElements());
             return books.stream()
                     .map(BookDto::toDto)
                     .collect(Collectors.toList());
@@ -52,10 +50,17 @@ public class BookController {
         return true;
     }
 
-    @GetMapping("/api/books_generate")
+    @PatchMapping("/api/books_generate")
     public boolean generateBooks(@RequestParam("count") int count) {
         List<Genre> genres = library.getGenres();
         List<Author> authors = library.getAuthors();
+        if (genres.size() == 0){
+            genres.add(new Genre ("drama"));
+        }
+        if (authors.size() == 0){
+            authors.add(new Author("Pushkin"));
+        }
+
         for (int i=0; i<count; i++) {
             library.addBook(UUID.randomUUID().toString(), authors.get((int)(authors.size()*Math.random())).getName(), genres.get((int)(genres.size()*Math.random())).getName());
         }
